4f38f581baceff15eb13330f521d7e2b5b3e58ff
Andi <andicbuchanan@gmail.com>
2016-01-25 17:00:39 +1300

    separate dropdowns

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var names = {"1954f":["Mary", "Elizabeth", "Sarah"], "1955f":["Jane", "Susan", "Clare"]};
-function getName(input) {
+function getName(year, gender) {
+  var input = year + gender;
   var item = names[input][Math.floor(Math.random()*3)];
   alert(item);
 }  
