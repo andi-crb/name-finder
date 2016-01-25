b7f940ee02ffcb1a216bb039814641d9f8dc57ee
Andi <andicbuchanan@gmail.com>
2016-01-25 15:20:32 +1300

    added html form elements

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var names = {"1954f":["Mary", "Elizabeth", "Sarah"], "1955f":["Jane", "Susan", "Clare"]};
-
-var input = prompt("please enter gender and year");
-var item = names[input][Math.floor(Math.random()*3)];
-alert(item);
+function getName(input) {
+  var item = names[input][Math.floor(Math.random()*3)];
+  alert(item);
+}  
